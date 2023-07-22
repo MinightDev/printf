04b2e1a6113e1c08d7a9f29d89ed28d343256389
MinightDev <onlyayoub565@gmail.com>
2023-07-22 13:48:02 -0700

    updated

--- 1-functions.c
+++ 1-functions.c
@@ -8,22 +8,7 @@
  */
 int _putchar(char c)
 {
-	static char buffer[BUFFER_SIZE];
-	static int i;
-	int j = 0;
-
-	if (c == '\n' || i >= BUFFER_SIZE - 1)
-	{
-		buffer[i] = c;
-		j = print_buffer(buffer, i + 1);
-		i = 0;
-	}
-	else
-	{
-		buffer[i++] = c;
-	}
-
-	return (j);
+	return (write(1, &c, 1));
 }
 /**
  * print_string - Prints a string to the standard output (stdout)
@@ -55,66 +40,30 @@ int print_string(const char *str)
 int print_int(int num)
 {
 	int i = 0;
-	int divi = 1;
-	int minus = 0;
+	int j = 1;
+	int negative = 0;
 
 	if (num == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-	if (num == INT_MIN)
-	{
-		_putchar('-');
-		i++;
-		num = -(num + 1);
-		minus = 1;
-	}
 	if (num < 0)
 	{
 		_putchar('-');
 		i++;
 		num = -num;
-		minus = 1;
+		negative = 1;
 	}
-	while (num / divi >= 10)
-		divi *= 10;
+	while (num / j >= 10)
+		j *= 10;
 
-	while (divi != 0)
+	while (j > 0)
 	{
-		_putchar(num / divi + '0');
+		_putchar(num / j + '0');
 		i++;
-		num %= divi;
-		divi /= 10;
+		num %= j;
+		j /= 10;
 	}
-	return (i + minus);
-}
-/**
- * print_buffer - Writes a buffer to the standard output (stdout)
- * @buffer: The buffer containing the data to be written
- * @len: The length of the data in the buffer
- *
- * Return: The number of characters written
- */
-int print_buffer(char *buffer, int len)
-{
-	int i = 0;
-
-	/* Write data in chunks of BUFFER_SIZE */
-	while (len > 0)
-	{
-		int size;
-
-		if (len > BUFFER_SIZE)
-		{
-			size = BUFFER_SIZE;
-		} else
-		{
-			size = len;
-		}
-		i += write(1, buffer, size);
-		buffer = buffer + size;
-		len = len - size;
-	}
-	return (i);
+	return (i + negative);
 }
