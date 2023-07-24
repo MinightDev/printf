9ae6bca116d591bc7d99a9badc0fd2b6826f1804
MinightDev <onlyayoub565@gmail.com>
2023-07-24 14:13:57 -0700

    mybe

--- handlers.c
+++ handlers.c
@@ -99,8 +99,11 @@ int handle_specifiers(const char *format, va_list args)
 	}
 	else if (format[i] == 's')
 	{
-		const char *str = va_arg(args, const char *)
+		const char *str = va_arg(args, const char *);
 
+		if (str == NULL)
+			i += print_string("(null)");
+		else
 			i += print_string(str);
 	}
 	else if (format[i] == 'd' || format[i] == 'i')
